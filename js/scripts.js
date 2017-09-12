d59e2736ee5ed9bf2b730cd9b09d53a6364464fd
brandon kudo and kyle ebersole <brandon+kyle@>
2017-09-12 14:12:12 -0700

    Create project directory

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+  $("#click1").click(function() {
+    $("#hide1").slideToggle();
+    $("#li1").slideToggle();
+  });
+
+  $("#click2").click(function() {
+    $("#hide2").slideToggle();
+    $("#li2").slideToggle();
+  });
+
+  $("#click3").click(function() {
+    $("#hide3").slideToggle();
+    $("#li3").slideToggle();
+  });
+
+
+  $("#click4").click(function() {
+    $("#hide4").slideToggle();
+    $("#li4").slideToggle();
+  });
+
+
+  $("#click5").click(function() {
+    $("#hide5").slideToggle();
+    $("#li5").slideToggle();
+  });
+
+  $("#click6").click(function() {
+    $("#hide6").slideToggle();
+    $("#li6").slideToggle();
+  });
+
+  $("#click7").click(function() {
+    $("#hide7").slideToggle();
+    $("#li7").slideToggle();
+  });
+
+  $("#click8").click(function() {
+    $("#hide8").slideToggle();
+    $("#li8").slideToggle();
+  });
+
+  $("#click9").click(function() {
+    $("#hide9").slideToggle();
+    $("#li9").slideToggle();
+  });
+
+  $("#click10").click(function() {
+    $("#hide10").slideToggle();
+    $("#li10").slideToggle();
+  });
+
+
+});
+
+// $("hide1").click(function() {
+//   $("hide1").slideToggle();
+//   $("li1").slideToggle("hide");
